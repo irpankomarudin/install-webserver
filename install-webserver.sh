12762812ccbd9cb1a5041183e0548c659ecef52f
irpankomarudin <irpankomarudin10@gmail.com>
2020-08-09 05:48:08 +0700

    Revisi Code

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-jawaban="Y“
+jawaban="Y"
 
-read -p "Lanjutkan Install webserver ? (Y/n) " pilih;
+read -p "Lanjutkan Installasi webserver ? (Y/n) " pilih;
 
-if [ $pilih == $jawaban ]; 
-then    
+if [ $pilih == $jawaban ];
+then
 	echo "Menyiapkan Installasi Web server"
-	echo "Melakukan Update"  
-	sudo apt-get update    
-	echo "Melakukan Installasi Webserver"    
-	sudo apt-get install -y apache2 php php-mysql    
-	echo "Melakukan Installasi Database Server"    
-	sudo apt-get install -y mysql-server    
+	echo "Melakukan Update"
+	sudo apt-get update
+	echo "Update Selesai"
+	echo "Melakukan Installasi Webserver"
+	sudo apt-get install -y apache2 php php-mysql
+	echo "Melakukan Installasi Database Server"
+	sudo apt-get install -y mysql-server
 	echo "Installasi Selesai"
 	echo "Versi Php adalah:"
 	php -v
-	echo "Versi MySQL"
-	mysql -v   
-	exit 0 
+	echo "Versi MySQL adalah:"
+	mysql -V
+	exit 0
 else    
 	echo "Installasi dibatalkan"    
 	exit 1 
